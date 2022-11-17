5769b07f49d69f8b949fbd5423ad8e6c78f44881
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-11-17 10:50:49 -0500

    basic express app

---
+++ server.js
@@ -0,0 +1,35 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+// HOROKU HAS A ENVORIMENT VARIABLE, and port is a port that horoku will set up... so we create a variable PORT 3000, so if im running locally it runs on PORT 3000 but if its on horoku we assing PORT=process.env.PORT
+let PORT= 3000
+if(process.env.PORT){
+    PORT=process.env.PORT
+}
+
+
+app.get('/', (req, res) => {
+    res.send('hi');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==================== my local port 
+
+app.listen(PORT, () =>{
+    console.log('listening...');
+})
