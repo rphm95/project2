687d0c0f692fd8833083d4c7b0ed63f394ebd72c
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-11-25 17:15:35 -0500

    some css

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const app = express();
 const seedLocation = require('./models/travelSeed.js')
 const seedAttractions = require('./models/attractionsSeed.js')
 
+
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
-
 app.use(express.json());
 
 const locationsController = require('./controllers/locations.js');
@@ -19,9 +20,11 @@ app.use(locationsController);
 const attractionsController = require('./controllers/attractions.js');
 app.use(attractionsController);
 
+app.use(express.static('public'))
 
-// const Location = require('./models/travelSchema.js') // --- on controllers folder
-// const Attractions = require('./models/attractions.js'); // --- on controllers folder
+
+const Location = require('./models/travelSchema.js') // --- on controllers folder
+const Attractions = require('./models/attractions.js'); // --- on controllers folder
 // const Attraction = require('./models/attractions.js');
 
 // HOROKU HAS A ENVORIMENT VARIABLE, and port is a port that horoku will set up... so we create a variable PORT 3000, so if im running locally it runs on PORT 3000 but if its on horoku we assing PORT=process.env.PORT
@@ -41,8 +44,8 @@ app.get('/seed', (req, res) => {
             food: ["Feijoada", "Moqueca"],
             flightPrice: "1000",
             attractions: [],
-            hotelPrice: "150/night",
-            description: "the most attractive tourist city in South America, a place that is a symbol of fun, games, great atmosphere. Today, the former capital of Brazil and the Portuguese Empire is the center of the most famous carnival in the world. This destination guards the most giant statue of Christ and boasts one of the most famous beaches in the world: Copacabana. Our site is dedicated to a detailed description of all the most important parts of travel to this fabulous city.",
+            hotelPrice: "150",
+            description: "The most attractive tourist city in South America, a place that is a symbol of fun, games, great atmosphere. Today, the former capital of Brazil and the Portuguese Empire is the center of the most famous carnival in the world. This destination guards the most giant statue of Christ and boasts one of the most famous beaches in the world: Copacabana. Our site is dedicated to a detailed description of all the most important parts of travel to this fabulous city.",
             bookingLink: "https://www.expedia.com/Cheap-Flights-To-Rio-De-Janeiro.d178301.Travel-Guide-Flights"
         },
         (error, data) => {}
@@ -56,7 +59,7 @@ app.get('/seed', (req, res) => {
             food: ["Croissants", "Brioches"],
             flightPrice: "1000",
             attractions: [],
-            hotelPrice: "150/night",
+            hotelPrice: "150",
             description: "Paris was founded in the 3rd century B.C. on île de la Cité by a community of Celts. They were a group of tribal fishermen called the Parisii who, pushed by emigration towards the banks of the Seine, made a permanent settlement there and profited from the area’s fertility and temperate climate. Furthermore, the islands on the Seine seemed the perfect place for this little community to establish their capital.",
             bookingLink: "https://www.expedia.com/Cheap-Flights-To-Rio-De-Janeiro.d178301.Travel-Guide-Flights"
     
@@ -72,7 +75,7 @@ app.get('/seed', (req, res) => {
             food: ["Pizza", "Baggle"],
             flightPrice: "200",
             attractions: [],
-            hotelPrice: "250/night",
+            hotelPrice: "250",
             description: "One of the greatest cities in the world, New York is always a whirlwind of activity, with famous sites at every turn and never enough time to see them all. Some people come here to enjoy the Broadway shows; others come specifically to shop and dine; and many come simply to see the sites: the Statue of Liberty, Empire State Building, Brooklyn Bridge, Central Park, historic neighborhoods, and numerous world famous museums.",
             bookingLink: "https://www.expedia.com/Cheap-Flights-To-New-York.d178293.Travel-Guide-Flights"
         },
